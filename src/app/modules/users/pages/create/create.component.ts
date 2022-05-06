505dcc8c1e2457afa8eab08695c9496de07fcfcd
Alejandro Hernández <j.ahr@hotmail.com>
2022-05-06 16:36:56 -0500

    loading users

--- src/app/modules/users/pages/create/create.component.ts
+++ src/app/modules/users/pages/create/create.component.ts
@@ -12,6 +12,7 @@ export class CreateComponent implements OnInit {
   formUser!: FormGroup;
   submitted: boolean = false;
   stored: boolean = false;
+  spinner: boolean = false;
 
   constructor(
     private formBuilder: FormBuilder,
@@ -19,12 +20,12 @@ export class CreateComponent implements OnInit {
   ) {
     this.formUser = this.formBuilder.group({
       id: [0, Validators.required],
-      name: ['', [Validators.required, Validators.minLength(10)]],
-      surname: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]],
-      phone: ['', [Validators.required, Validators.maxLength(15)]],
-      username: ['', Validators.required],
-      password: ['', Validators.maxLength],
+      name: ['John aLEJANDRO hERNANDE', [Validators.required, Validators.minLength(10)]],
+      surname: ['ERKGJRT', Validators.required],
+      email: ['j.ajr@gmail.com', [Validators.required, Validators.email]],
+      phone: ['dfer', [Validators.required, Validators.maxLength(15)]],
+      username: ['username12', Validators.required],
+      password: ['123qweasd', Validators.maxLength],
     })
   }
 
@@ -32,19 +33,21 @@ export class CreateComponent implements OnInit {
   }
 
   onSubmit(): void {
+    this.spinner = true;
     console.log(this.formUser.controls);
     this.submitted = true;
 
     if (this.formUser.valid) {
-
       this.userService.storeUser(this.formUser.value).subscribe(data => {
-        console.log(data);
-        this.stored = data.success;
+          console.log(data);
+          this.stored = data.success;
+          this.spinner = false;
       })
 
       console.log(this.formUser.value, 'se ve bien!')
     } else {
-      console.log("FILL ALL FIELDS")
+        console.log("FILL ALL FIELDS")
+        this.spinner = false;
     }
   }
 
